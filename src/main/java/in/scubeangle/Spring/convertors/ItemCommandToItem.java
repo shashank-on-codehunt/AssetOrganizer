2f75f3d48d0fede77b7f8c18179e263d97512f60
Shashank <shashank4387@gmail.com>
2021-01-05 23:33:30 +0530

    update for Tags dublicate input

--- src/main/java/in/scubeangle/Spring/convertors/ItemCommandToItem.java
+++ src/main/java/in/scubeangle/Spring/convertors/ItemCommandToItem.java
@@ -4,6 +4,7 @@ import in.scubeangle.Spring.commands.ItemCommand;
 import in.scubeangle.Spring.commands.TagCommand;
 import in.scubeangle.Spring.domains.Item;
 import in.scubeangle.Spring.domains.Tag;
+import in.scubeangle.Spring.repository.TagRepository;
 import lombok.AllArgsConstructor;
 import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
@@ -19,6 +21,7 @@ import java.util.stream.Collectors;
 public class ItemCommandToItem implements Converter<ItemCommand, Item> {
 
     private final TagCommandToTag tagCommandToTag;
+    private final TagRepository tagRepository;
 
     @Synchronized
     @Nullable
@@ -32,7 +35,7 @@ public class ItemCommandToItem implements Converter<ItemCommand, Item> {
         item.setItemName(source.getItemName());
         item.setParentItemName(source.getParentItemName());
         item.setPriority(source.getPriority());
-        item.setTags(Arrays.stream(source.getTag().split(";")).map(o -> tagCommandToTag.convert(new TagCommand(o))).collect(Collectors.toSet()));
+        item.setTags(Arrays.stream(source.getTag().split(";")).map(o -> tagRepository.findByTagName(o).orElseGet(() -> tagCommandToTag.convert(new TagCommand(o)))).collect(Collectors.toSet()));
         return item;
     }
 }
